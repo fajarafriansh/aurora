6422ff1a9ca34ae4db66648c97279c85b5722b94
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-07-26 04:29:48 +0700

    add blog

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,9 @@
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload };
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
+      };
+  },
+};
