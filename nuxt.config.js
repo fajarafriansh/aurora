5c4317f8d334abcefa84abdef23f48b7ac218ce6
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-07-26 13:23:50 +0700

    netlify cms configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,8 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/markdownit'
   ],
   /*
   ** Axios module configuration
@@ -64,9 +65,28 @@ export default {
   */
   axios: {},
   /*
+  ** Markdownit module configuration
+  */
+  markdownit: {
+    injected: true,
+  },
+  /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
   }
+
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./assets/content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./assets/content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -1,9 +1,18 @@
-export default {
-  async asyncData({ params, payload }) {
-    if (payload) return { blogPost: payload };
-    else
-      return {
-        blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
-      };
-  },
-};
+<template>
+  <article>
+    <h1>{{blogPost.title}}</h1>
+    <div v-html="$md.render(blogPost.body)" />
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params, payload }) {
+      if (payload) return { blogPost: payload };
+      else
+        return {
+          blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
+        };
+    },
+  };
+</script
