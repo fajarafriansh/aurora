22d1201930ab15ab839c95c973a06d2b09dd19f0
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-09-29 18:09:27 +0700

    add color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#212429',
+        'green': '#00A37A',
+        violet: {
+          '5': '#7048E8',
+          '4': '#845EF7',
+          '3': '#9775FA',
+          '2': '#B197FC',
+          '1': '#D0BFFF',
+        },
+      },
+      spacing: {
+        '100': '25rem',
+      },
+    },
+    container: {
+      padding: {
+        default: '1rem',
+        md: '3rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
