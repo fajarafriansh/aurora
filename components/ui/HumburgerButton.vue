53798745c0b3f0a6d257b7b83969041cd7866ec9
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-09-29 18:13:09 +0700

    add humberger button

---
+++ components/ui/HumburgerButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <svg
+      data-v-9baaa708=""
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      role="presentation"
+      preserveAspectRatio="xMidYMid meet"
+      fill="none"
+      class="fill-current stroke-1 w-8 icon stroke-current h-auto"
+    >
+      <rect
+        data-v-9baaa708=""
+        width="24"
+        height="3"
+        x="4"
+        y="7"
+        rx="1.5"
+      ></rect>
+      <rect
+        data-v-9baaa708=""
+        width="24"
+        height="3"
+        x="4"
+        y="22"
+        rx="1.5"
+      ></rect>
+    </svg>
+  </div>
+</template>
