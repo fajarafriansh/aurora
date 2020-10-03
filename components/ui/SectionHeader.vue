6b2d5ef8400825569bcf92332c2e2e68ee27b82b
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-10-03 16:46:57 +0700

    add section header component

---
+++ components/ui/SectionHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="block h-auto">
+    <div class="flex border-b-2 border-gray-300">
+      <h1 class="text-xl py-4 font-semibold border-b-2 border-black -mb-0.5">
+        <slot></slot>
+      </h1>
+    </div>
+  </div>
+</template>
