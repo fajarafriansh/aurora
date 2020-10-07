9618f03b96fb561466f654f0bd00c82d66bd6b23
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-10-07 08:44:46 +0700

    fixing index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="mt-20 lg:mt-32">
     <div class="container mx-auto">
-      <Hero
-        :key="articles[1].title"
-        :title="articles[1].title"
-        :cover="articles[1].cover"
-        :description="articles[1].description"
-        :author="articles[1].author"
-        :date="articles[1].date.toLocaleDateString()"
-      />
+      <section>
+        <Hero
+          :key="articles[1].title"
+          :title="articles[1].title"
+          :cover="articles[1].cover"
+          :description="articles[1].description"
+          :author="articles[1].author"
+          :date="articles[1].date.toLocaleDateString()"
+        />
+      </section>
       <div class="mt-10 lg:flex lg:space-x-8">
         <section class="lg:w-2/3">
           <section-header>All Stories</section-header>
@@ -22,6 +24,11 @@
               :author="article.author"
               :date="article.date.toLocaleDateString()"
             />
+          </div>
+          <div class="flex justify-center mt-12">
+            <aurora-link :to="`/slug`" as="link">
+              <span class="flex-1 py-1 border-b-3">See all stories</span>
+            </aurora-link>
           </div>
         </section>
         <aside class="lg:w-1/3 mt-8 lg:mt-0">
@@ -38,6 +45,7 @@
         </aside>
       </div>
     </div>
+    <NewsletterSection />
   </main>
 </template>
 
@@ -45,6 +53,28 @@
 export default {
   asyncData() {
     const articles = [
+      {
+        title: 'How to make your articles Vuetiful, and its so ease',
+        cover: '/img.jpg',
+        description:
+          'This article guides you through all the steps to make an article shine with your favourite framework, Vue',
+        author: {
+          name: 'Naruto Uzumaki',
+          image: '/oni.jpg',
+        },
+        date: new Date(),
+      },
+      {
+        title: 'How to make your articles Vuetiful',
+        cover: '/img.jpg',
+        description:
+          'This article guides you through all the steps to make an article shine with your favourite framework, Vue',
+        author: {
+          name: 'Naruto Uzumaki',
+          image: '/oni.jpg',
+        },
+        date: new Date(),
+      },
       {
         title: 'How to make your articles Vuetiful',
         cover: '/img.jpg',
