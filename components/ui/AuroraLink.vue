ac91aa7d7f3305466abebf49c31e64f4b5eb9fd5
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-10-07 08:42:02 +0700

    add link component

---
+++ components/ui/AuroraLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <nuxt-link
+      :to="to"
+      class="px-4 py-2 font-semibold transition ease-in-out duration-150"
+      :class="linkClases"
+    >
+      <slot></slot>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: null,
+    },
+    as: {
+      type: String,
+      default: 'link',
+    },
+  },
+  computed: {
+    linkClases() {
+      if (this.as === 'link') {
+        return 'text-green text-lg hover:text-violet-4'
+      } else {
+        return 'text-white bg-green text-lg rounded-full hover:bg-violet-4'
+      }
+    },
+  },
+}
+</script>
