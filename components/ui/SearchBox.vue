3c6e6f80429bc61db7f7d3122209f9bb0fdddc24
Fajar Afriansh <fajar.afrianshah@gmail.com>
2020-12-02 10:12:13 +0700

    algolia instant search

--- components/ui/SearchBox.vue
+++ components/ui/SearchBox.vue
@@ -1,26 +1,94 @@
 <template>
-  <div
-    class="relative text-grayscale-5 dark:text-grayscale-4 text-sm lg:text-base"
-  >
-    <div
-      class="flex items-center justify-center w-12 absolute inset-0 py-2 px-3"
+  <client-only>
+    <ais-instant-search
+      v-click-outside="closeAutocomplete"
+      :search-client="searchClient"
+      index-name="netlify_aurora"
     >
-      <unicon name="search" />
-    </div>
-    <input
-      ref="search"
-      v-model="searchInput"
-      type="search"
-      placeholder="Search (Press ''/'' to focus)"
-      class="w-full bg-grayscale-1 dark:bg-grayscale-6 rounded-md border-2 border-grayscale-3 dark:border-grayscale-5 placeholder-current outline-none focus:border-primary-2 dark-focus:border-primary-1 pl-12 pr-4 py-2"
-      @focus="isFocus = true"
-      @blur="isFocus = false"
-      @keydown.esc="blurInput"
-    />
-  </div>
+      <ais-autocomplete>
+        <div slot-scope="{ indices, refine }">
+          <div>
+            <input
+              v-model="keywords"
+              type="search"
+              placeholder="Search"
+              @input="refine($event.currentTarget.value)"
+            >
+            <div v-if="keywords && !hideAutocomplete">
+              <a 
+                v-for="hit in indices[0].hits"
+                :key="hit.objectID"
+                @click="goToBlog(hit.objectID)"
+              >
+                <ais-highlight attribute="title" :hit="hit" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </ais-autocomplete>
+    </ais-instant-search>
+    <!-- <div
+      class="relative text-grayscale-5 dark:text-grayscale-4 text-sm lg:text-base"
+    >
+      <div
+        class="flex items-center justify-center w-12 absolute inset-0 py-2 px-3"
+      >
+        <unicon name="search" />
+      </div>
+      <input
+        ref="search"
+        v-model="searchInput"
+        type="search"
+        placeholder="Search (Press ''/'' to focus)"
+        class="w-full bg-grayscale-1 dark:bg-grayscale-6 rounded-md border-2 border-grayscale-3 dark:border-grayscale-5 placeholder-current outline-none focus:border-primary-2 dark-focus:border-primary-1 pl-12 pr-4 py-2"
+        @focus="isFocus = true"
+        @blur="isFocus = false"
+        @keydown.esc="blurInput"
+      />
+    </div> -->
+    
+  </client-only>
 </template>
 
 <script>
+import algoliasearch from 'algoliasearch/lite'
+
+export default {
+  data () {
+    return {
+      searchClient: algoliasearch(
+        process.env.algoliaAppId,
+        process.env.algoliaSearchApiKey
+      ),
+      keywords: '',
+      hideAutocomplete: false
+    }
+  },
+  watch: {
+    keywords (value) {
+      if (value) {
+        this.hideAutocomplete = false
+      }
+    }
+  },
+  methods: {
+    goToBlog (blogId) {
+      this.keywords = ''
+      this.$router.push({
+        name: 'blog-id',
+        params: {
+          id: blogId
+        }
+      })
+    },
+    closeAutocomplete () {
+      this.hideAutocomplete = true
+    }
+  }
+}
+</script>
+
+<!-- <script>
 export default {
   data: () => ({
     searchInput: '',
@@ -46,4 +114,4 @@ export default {
     },
   },
 }
-</script>
+</script> -->
--- nuxt.config.js
+++ nuxt.config.js
@@ -41,10 +41,11 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/filters.js',
+    '~/plugins/filters',
     { src: '~/plugins/vue-unicons', mode: 'client' },
-    '~/plugins/vue-js-modal.js',
+    '~/plugins/vue-js-modal',
     '~/plugins/disqus',
+    '~/plugins/vue-instantsearch'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -65,9 +66,7 @@ export default {
   // modules: ['@nuxtjs/apollo', '@nuxtjs/markdownit', '@nuxtjs/sitemap'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    transpile: ['vue-instantsearch', 'instantsearch.js/es'],
-  },
+  build: {},
 
   purgeCSS: {
     whitelist: ['dark-mode'],
