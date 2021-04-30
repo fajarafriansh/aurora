a5c596cfd5cf47eb3c4ed1dad0147b60c4859597
Fajar Afriansh <fajar.afrianshah@gmail.com>
2021-05-01 02:58:15 +0700

    Update medium-zoom.js

--- plugins/medium-zoom.js
+++ plugins/medium-zoom.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import mediumZoom from 'medium-zoom'
 
-const initZoom = () => {
-  mediumZoom('#app img:not(.medium-zoom-image)')
-}
-
-Vue.mixin({
+new Vue({
   mounted() {
-    initZoom()
-  },
-  updated() {
-    initZoom()
-  },
+    mediumZoom('.prose img:not(.medium-zoom-image)')
+  }
 })
+
+// const initZoom = () => {
+//   mediumZoom('#app img:not(.medium-zoom-image)')
+// }
+
+// Vue.mixin({
+//   mounted() {
+//     initZoom()
+//   },
+//   updated() {
+//     initZoom()
+//   },
+// })
